347bd60a5169f37c6bcda9729ccc35d597c00bd5
Rahul <rahulkumarsui554@gmail.com>
2025-09-02 13:04:28 +0530

    news

--- src/hooks/useNewApi.js
+++ src/hooks/useNewApi.js
@@ -2,11 +2,10 @@ import React, { useState, useEffect } from 'react'
 const useNewApi = (newstype) => {
   const [dataa, setData] = useState([]) 
   useEffect(() => {
-    fetch(`${import.meta.env.VITE_APIURL1}category=${newstype}${import.meta.env.VITE_APIURL2}`)
+    fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${newstype}&apiKey=${import.meta.env.VITE_APIURL}`)
     .then((res)=>res.json())
     .then((data)=> setData(data.articles))    
   }, [newstype])
   return dataa
 }
 export default useNewApi
-  // fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${newstype}&apiKey=f92dce6d994642bcaddffd2bd7fe8f47`)
